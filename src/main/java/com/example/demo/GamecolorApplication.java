a8bc9df99945b7e1c7c37bf3c500b9fe360475ed
dharuni03 <727721euit030@skcet.ac.in>
2023-02-22 12:58:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/GamecolorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecolorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecolorApplication.class, args);
+	}
+
+}
